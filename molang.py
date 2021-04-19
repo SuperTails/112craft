c1be5255c54aa374df8d9c91dc4849930d15d27f
Carson Swoveland <sciencedude2003@gmail.com>
2021-04-19 10:33:07 -0600

    Molang improvements

--- molang.py
+++ molang.py
@@ -1,4 +1,4 @@
-from typing import Union, List, Tuple, Optional
+from typing import Union, List, Tuple, Optional, Any
 from dataclasses import dataclass
 import operator
 import math
@@ -39,17 +39,59 @@ def lex(expr: str) -> List[Token]:
             if current is None:
                 current = ''
             current += c
-        elif c.isalnum() or c == '_':
+        elif c == '&':
+            if current is not None and current != '&':
+                result.append(current)
+                current = None
+            
             if current is None:
                 current = ''
 
-            current += c
-        elif c in ('+', '-', '*', '/'):
+            current += '&'
+            
+            if current == '&&':
+                result.append('&&')
+                current = None
+        elif c == '|':
+            if current is not None and current != '|':
+                result.append(current)
+                current = None
+            
+            if current is None:
+                current = ''
+
+            current += '|'
+            
+            if current == '||':
+                result.append('||')
+                current = None
+        elif c == '=':
+            if current is not None and current != '=':
+                result.append(current)
+                current = None
+            
+            if current is None:
+                current = ''
+
+            current += '='
+            
+            if current == '==':
+                result.append('==')
+                current = None
+        elif c in ('+', '-', '*', '/', ',', '?', ':', '<', '>'):
             if current is not None:
                 result.append(current)
                 current = None
 
             result.append(c)
+        elif c.isalnum() or c == '_':
+            if current is None:
+                current = ''
+            if '=' in current or '&' in current or '|' in current:
+                result.append(current)
+                current = ''
+
+            current += c
         else:
             raise Exception(c)
     
@@ -114,9 +156,18 @@ def parse(tokens: TokenIter, min_pow=0):
             tokens.next()
 
             if op == '(':
-                rhs = parse(tokens, 0)
-                assert(tokens.next() == ')')
-                lhs = (lhs, (rhs, ))
+                rhs = []
+
+                while True:
+                    rhs.append(parse(tokens, 0))
+                    nextTok = tokens.next()
+                    if nextTok == ',':
+                        pass
+                    elif nextTok == ')':
+                        break
+                    else:
+                        raise Exception(nextTok)
+                lhs = (lhs, tuple(rhs))
             else:
                 lhs = (op, (lhs, ))
 
@@ -130,8 +181,14 @@ def parse(tokens: TokenIter, min_pow=0):
         
             tokens.next()
 
-            rhs = parse(tokens, rightIfp)
-            lhs = (op, (lhs, rhs))
+            if op == '?':
+                mhs = parse(tokens, 0)
+                assert(tokens.next() == ':')
+                rhs = parse(tokens, rightIfp)
+                lhs = (op, (lhs, mhs, rhs))
+            else:
+                rhs = parse(tokens, rightIfp)
+                lhs = (op, (lhs, rhs))
 
             continue
     
@@ -140,20 +197,30 @@ def parse(tokens: TokenIter, min_pow=0):
     return lhs
 
 def prefix_power(c: Token) -> Optional[int]:
+    if c == 'return':
+        return 1
     if c in ('+', '-'):
-        return 9
+        return 17
     else:
         return None
 
 def postfix_power(c: Token) -> Optional[int]:
     if c == '(':
-        return 7
+        return 15
 
 def infix_power(c: Token) -> Optional[Tuple[int, int]]:
-    if c in ('+', '-'):
-        return (1, 2)
+    if c == '?':
+        return (4, 3)
+    elif c == '||':
+        return (5, 6)
+    elif c == '&&':
+        return (7, 8)
+    elif c in ('<', '>', '=='):
+        return (9, 10)
+    elif c in ('+', '-'):
+        return (11, 12)
     elif c in ('*', '/'):
-        return (3, 4)
+        return (13, 14)
     else:
         return None
 
@@ -164,43 +231,89 @@ def printTree(p, indent=0):
     else:
         print(f"{' '*indent}{p}")
 
-def evalString(s, entity):
-    parsed = parse(TokenIter(lex(s), 0))
-    return evalExpr(parsed, entity)
+def parseStr(s: str) -> List[Any]:
+    if ';' in s:
+        result = []
+        for line in s.split(';')[:-1]:
+            result.append(parse(TokenIter(lex(line), 0)))
+        return result
+    else:
+        return [parse(TokenIter(lex(s), 0))]
 
+def evalString(s, entity):
+    try:
+        assert(';' not in s)
+        parsed = parseStr(s)
+        result = evalOneExpr(parsed[0], entity)
+        assert(result is not None)
+        return result
+    except Exception as e:
+        print(s)
+        print(lex(s))
+        raise Exception((e, s))
+    
 def evalExpr(p, entity):
+    for expr in p[:-1]:
+        evalOneExpr(expr, entity)
+    
+    return evalOneExpr(p[-1], entity)
+
+def evalOneExpr(p, entity):
     def evalApplied(p):
-        return evalExpr(p, entity)
+        return evalOneExpr(p, entity)
 
     if isinstance(p, tuple):
         (op, args) = p
 
+        if op == '=':
+            assert(args[0].startswith('variable'))
+            entity.variables[args[0].removeprefix('variable')] = evalApplied(args[1])
+            return None
+
         args = tuple(map(evalApplied, args))
 
-        UNARY_OPS = {
-            '+': lambda x: x,
-            '-': lambda x: -x,
-            'math.sin': lambda x: math.sin(math.radians(x)),
-            'math.cos': lambda x: math.cos(math.radians(x)),
-        }
+        OPS = [
+            # Unary
+            {
+                '+': lambda x: x,
+                '-': lambda x: -x,
+                'math.sin': lambda x: math.sin(math.radians(x)),
+                'math.cos': lambda x: math.cos(math.radians(x)),
+                'math.sqrt': math.sqrt,
+                'math.round': round,
+                'query.position_delta': lambda x: entity.getQuery(f'position_delta{int(x)}'),
+                'return': lambda x: x,
+            },
+            # Binary
+            {
+                '+': operator.add,
+                '-': operator.sub,
+                '*': operator.mul,
+                '/': operator.truediv,
+                '<': operator.lt,
+                '>': operator.gt,
+                '&&': lambda a, b: 1.0 if a != 0 and b != 0 else 0.0,
+                '||': lambda a, b: 1.0 if a != 0 or b != 0 else 0.0,
+                '==': lambda a, b: 1.0 if a == b else 0.0,
+                'math.pow': math.pow,
+                'math.mod': lambda x, y: x % y,
+            },
+            # Ternary
+            {
+                'math.lerp': lambda a, b, t: a * (1 - t) + b * t,
+                'math.clamp': lambda x, l, h: max(min(x, h), l),
+                '?': lambda c, t, f: t if c != 0 else f,
+            },
+        ]
 
-        BINARY_OPS = {
-            '+': operator.add,
-            '-': operator.sub,
-            '*': operator.mul,
-            '/': operator.truediv,
-        }
-
-        if len(args) == 1:
-            ops = UNARY_OPS
-        elif len(args) == 2:
-            ops = BINARY_OPS
-        else:
+        try:
+            ops = OPS[len(args) - 1]
+        except IndexError:
             raise Exception(args)
 
         return ops[op](*args)
-    elif isinstance(p, float):
-        return p
+    elif isinstance(p, float) or isinstance(p, int):
+        return float(p)
     elif p.startswith('query.'):
         p = p.removeprefix('query.')
         return entity.getQuery(p)
@@ -208,11 +321,14 @@ def evalExpr(p, entity):
         p = p.removeprefix('variable.')
         if p not in entity.variables:
             print(f"UNKNOWN VARIABLE {p}")
-            entity.variables[p] = 0.0
+            if p == 'gliding_speed_value':
+                entity.variables[p] = 1.0
+            else:
+                entity.variables[p] = 0.0
         
         return entity.variables[p]
     elif p == 'this':
         # TODO:
         return 0.0
     else:
-        raise Exception(p)
+        raise Exception(repr(p))
